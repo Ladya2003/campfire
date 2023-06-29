a37d154d83b3fc939355065d7fe11a4a32f4a2d1
Ladya2003 <vladiator_one@mail.ru>
2023-06-30 00:58:37 +0300

    add test html css js img vids stuf

---
+++ js/script.js
@@ -0,0 +1,30 @@
+// Функция, которая будет выполняться при нажатии на кнопку
+function playVideo() {
+
+    // Получаем ссылки на кнопку и видео
+    const button = document.querySelector('.header-button');
+    const video = document.querySelector('.bg-vid');
+    const header = document.querySelector('.header-container');
+    const header2 = document.querySelector('.header-container2');
+  
+    // Добавляем обработчик события при клике на кнопку
+    button.addEventListener('click', function() {
+      // Запускаем проигрывание видео
+      video.play();
+  
+      // Добавляем класс "filter-remove" к видео
+      video.classList.add('filter-remove');
+      header.classList.remove('animation-header-add');
+      header.classList.add('animation-header-add');
+
+      video.addEventListener('ended', function() {
+        // Добавляем класс "show-header" к блоку header-container
+        header.classList.add('header-container1');
+        header2.classList.add('show-header');
+      });
+    });
+  }
+  
+  // Вызываем функцию после полной загрузки страницы
+  window.addEventListener('load', playVideo);
+  
