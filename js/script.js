e4d0df4501b15b24ecbb171ce8acc144009b994b
Ladya2003 <vladiator_one@mail.ru>
2023-07-02 01:58:07 +0300

    change and add start page, change main nav page, add transition to blog
    changed starting page to a cloud type sht. then by pressing the btn bg vid change to a hogwarts gryffinder common room vid. and by pressing btn there bg vid change to a blog one

--- js/script.js
+++ js/script.js
@@ -1,28 +1,45 @@
 // Функция, которая будет выполняться при нажатии на кнопку
 function playVideo() {
 
-    // Получаем ссылки на кнопку и видео
-    const button = document.querySelector('.header-button');
-    const video = document.querySelector('.bg-vid');
-    const header = document.querySelector('.header-container');
-    const header2 = document.querySelector('.header-container2');
-  
-    // Добавляем обработчик события при клике на кнопку
-    button.addEventListener('click', function() {
-      // Запускаем проигрывание видео
-      video.play();
-  
-      // Добавляем класс "filter-remove" к видео
-      video.classList.add('filter-remove');
-      header.classList.remove('animation-header-add');
-      header.classList.add('animation-header-add');
+  const button = document.querySelector('.header-button');
+  const button2 = document.querySelector('.header-button2');
+  const video = document.querySelector('#bg-vid');
+  const video2 = document.querySelector('.bg-vid2');
+  const video3 = document.querySelector('.bg-vid3');
+  const header = document.querySelector('.header-container');
+  const header2 = document.querySelector('.header-container2');
+  const audio = document.querySelector('#myAudio');
+  const dragon = document.querySelector('.dragon');
 
-      video.addEventListener('ended', function() {
-        // Добавляем класс "show-header" к блоку header-container
-        header.classList.add('header-container1');
-        header2.classList.add('show-header');
-      });
+  video2.pause();
+
+  button.addEventListener('click', function() {
+    video.classList.add('bg-vid-dissapper');
+    header.classList.add('animation-header-add');
+    audio.play();
+    video2.play();
+    video2.classList.add('filter-remove');
+    dragon.classList.add('header-container1');
+
+
+    video2.addEventListener('ended', function() {
+      // Добавляем класс "show-header" к блоку header-container
+      header.classList.add('header-container1');
+      header2.classList.add('show-header');
+      audio.pause();
+      video2.classList.add('filter-add');
     });
+    button2.addEventListener('click', function() {
+      video2.classList.add('bg-vid-dissapper');
+      video3.play();
+      video3.classList.add('filter-remove');
+
+      video3.addEventListener('ended', function() {
+        video3.classList.add('filter-add');
+      });
+
+    });
+  });
   }
   
   // Вызываем функцию после полной загрузки страницы
